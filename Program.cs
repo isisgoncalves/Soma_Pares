599b071fbb3b6b4d916f401a63f7a92fbfc79b88
isisgoncalves <isisvow@gmail.com>
2024-02-02 07:11:35 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    int limiteInferior = int.Parse(Console.ReadLine());
+    int limiteSuperior = int.Parse(Console.ReadLine());
+
+    int somaPares = 0;
+
+    for (int i = limiteInferior; i <= limiteSuperior; i++)
+    {
+        if (i % 2 == 0)
+        {
+        somaPares += i;
+        }
+    }
+
+    Console.WriteLine($"A soma dos numeros pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
+    }
+}
